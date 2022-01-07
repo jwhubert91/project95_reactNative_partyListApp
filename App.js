154d9003086832cc7fdd79fd6f32e62f421a5804
James Hubert <jwhubert91@gmail.com>
2022-01-07 00:31:08 -0800

    Create party invites list app

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View, TextInput, Button, ImageBackground } from 'react-native';
+import partyImageTaller from './assets/partyImageTaller.jpeg';
 
 export default function App() {
+  const [invitees, setInvitees] = useState([]);
+  const [newInvitee, setNewInvitee] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.viewContainer}>
+      <View>
+        <Text style={styles.header}>React Native Party Invites App</Text>
+      </View>
+      <View style={styles.imageContainer}>
+        <ImageBackground source={partyImageTaller} resizeMode="cover" style={styles.partyImage}>
+          <Text style={styles.imageText}>Your Guest List</Text>
+        </ImageBackground>
+      </View>
+      <View style={ styles.inputContainer }>
+        <Text>New Invitee:</Text>
+        <TextInput
+          style={ styles.input }
+          value={ newInvitee }
+          onChangeText={setNewInvitee}
+          placeholder='Enter invitee here...'
+        />
+        <Button
+          title='Add new invitee'
+          onPress={ ()=> {
+            setInvitees([newInvitee, ...invitees])
+            setNewInvitee('')
+          } }
+        />
+      </View>
+      <View style={ styles.listContainer }>
+        <FlatList
+          data={ invitees }
+          keyExtractor={ invitee => invitee }
+          renderItem={({item})=> <Text style={ styles.inviteeText }>{ item }</Text>}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  viewContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  imageContainer: {
+    width: '100%',
     justifyContent: 'center',
+  },
+  partyImage: {
+    height: '300',
+  },
+  imageText: {
+    color: "white",
+    fontSize: 27,
+    lineHeight: 29,
+    fontWeight: "bold",
+    textAlign: "center",
+    // backgroundColor: "#000000c0",
+    paddingVertical: 50,
+  },
+  inputContainer: {
+    paddingVertical: 5,
+  },
+  header: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  listContainer: {
+    padding: 5,
+  },
+  inviteeText: {
+    fontSize: 18,
   },
 });
